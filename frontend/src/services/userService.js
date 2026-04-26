f3895f6340efab1c8c3bf2c6a55c1bc7591ad6cf
sree Nanda Sai <sreenandasai24@gmail.com>
2026-04-26 22:44:40 +0530

    Fix API base URL to fallback to localhost if VITE_API_URL not set

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,7 +3,7 @@ import authService from "./authService";
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
-const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+const API_BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`;
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
